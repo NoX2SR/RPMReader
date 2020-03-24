2eeb0b296d19c22f4a0a3779e659c64c55e4323d
NoX2SR <nemus92@gmail.com>
2020-03-24 18:12:03 +0100

    The finnal working version
    This is stabile working version.
    The future updates could be to determinate interrupt frequency more precisely.

--- RPMReader.ino
+++ RPMReader.ino
@@ -1,7 +1,7 @@
 
 #define inPin1 2 //pin connected to optocupler
 #define inPin2 3 //pin connected to read switch
-#define DEREControlPin 8 
+#define DEREControlPin 8
 #define debugPin 7
 
 #define sensorID 'Q'
@@ -10,37 +10,36 @@
 #define BoundRate 115200
 
 #define interuptFreq  8000 //Hz
+#define time2SetZero 1000
 
 //Variables for freq meter
 bool enteredFirstHigh;
 bool enteredSecondHigh;
-long time1 = 0;// time between one full rotation (in ms)
-long time2 = 0;
+long timer1 = 0;// time between one full rotation (in ms)
+long timer2 = 0;
 float freq1 = 0.00;
 float freq2 = 0.00;
+
+byte* toSend = new byte[4];
 
 char command = '1';
 
 bool interuptEntered = false;
 
-void setup() 
+void setup()
 {
-   pinMode(debugPin, OUTPUT);
+  pinMode(debugPin, OUTPUT);
   Serial.begin(BoundRate);
   pinMode(DEREControlPin, OUTPUT);//DE/RE Controling pin of RS-485
   digitalWrite(DEREControlPin, LOW); //DE/RE=LOW Receive Enabled M1
   pinMode(inPin1, INPUT);;//redd switch
   pinMode(inPin2, INPUT);;//redd switch
-  pinMode(13, OUTPUT);
+  //pinMode(13, OUTPUT);
+  InitInterrupt();
 }
 
 void loop()
 {
-  if(interuptEntered)
-  {
-    return;
-  }
-  
   if (tryGetSensorCommand())
   {
     executeCommand();
@@ -63,11 +62,11 @@ bool tryGetSensorCommand()
     return false;
   }
 
-  if(receivedBytes[0] != sensorID)
+  if (receivedBytes[0] != sensorID)
   {
     return false;
   }
-  
+
   if (receivedBytes[0] == 'Q')
   {
     command = receivedBytes[0];
@@ -85,18 +84,14 @@ void executeCommand()
   switch (command)
   {
     case 'Q': //Q:ǱB:ǱB:⸮⸮C:⸮⸮C::
-     Serial.print("InputPinStatus: \n");
-      Serial.print(digitalRead(inPin1));
-      Serial.print("\n");
-       Serial.print("InputPinStatus2: \n");
-      Serial.print(digitalRead(inPin2));
-      Serial.print("\n");
+      PrintFreq(freq1);
+      PrintFreq(freq2);
       break;
     case 'W':
-       PrintFreq1();
+      PrintFreq(freq1);
       break;
     case 'R':
-       PrintFreq2();
+      PrintFreq(freq2);
       break;
   }
   Serial.print(separator);
@@ -104,33 +99,87 @@ void executeCommand()
   digitalWrite(DEREControlPin, LOW); //DE/RE=LOW Receive Enabled
 }
 
-void PrintFreq1()
+void PrintFreq(float freq)
 {
-  //digitalWrite(debugPin, HIGH);
-  byte* b = (byte *) &freq1;
-  
-  Serial.write(b, 4);
+  toSend[0] = ~ ((byte *) &freq)[0];
+  toSend[1] = ~ ((byte *) &freq)[1];
+  toSend[2] = ~ ((byte *) &freq)[2];
+  toSend[3] = ~ ((byte *) &freq)[3];
+  Serial.write(toSend, 4);
   Serial.print(separator);
   //Twice to chack if there is error in received data
-  Serial.write(b, 4);
+  Serial.write(toSend, 4);
   Serial.print(separator);
-  //digitalWrite(debugPin, LOW);
 }
 
-void PrintFreq2()
+void InitInterrupt()
 {
-  //digitalWrite(debugPin, HIGH);
- 
-  byte* b = (byte *) &freq2;
-  
-  Serial.write(b, 4);
-  Serial.print(separator);
-  //Twice to chack if there is error in received data
-  Serial.write(b, 4);
-  Serial.print(separator);
-  //digitalWrite(debugPin, LOW);
+  //dissable interrupts
+  cli();
+  //set timer2 interrupt at 8kHz
+  TCCR2A = 0;// set entire TCCR2A register to 0
+  TCCR2B = 0;// same for TCCR2B
+  TCNT2  = 0;//initialize counter value to 0
+  // set compare match register for 8khz increments
+  OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set CS21 bit for 8 prescaler
+  TCCR2B |= (1 << CS21);
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
+  // Enable global interrupts: Ready to run!
+  sei();
 }
 
+ISR(TIMER2_COMPA_vect)
+{
+  if (digitalRead(inPin1) == HIGH)
+  {
+    if (enteredFirstHigh)
+    {
+      enteredFirstHigh = false;
+      freq1 = interuptFreq / timer1;
+      //freq1 = timer1;
+      timer1 = 0;
+    }
+    timer1 += 1;
+  }
+  else
+  {
+    if (!enteredFirstHigh)
+    {
+      enteredFirstHigh = true;
+    }
+    timer1 += 1;
+  }
 
+  if (timer1 > time2SetZero)
+  {
+    freq1 = 0;//if no new pulses from reed switch- tire is still, set Freq to 0
+  }
 
+  if (digitalRead(inPin2) == HIGH)
+  {
+    if (enteredSecondHigh)
+    {
+      enteredSecondHigh = false;
+      freq2 = interuptFreq / timer2;
+      timer2 = 0;
+    }
+    timer2 += 1;
+  }
+  else
+  {
+    if (!enteredSecondHigh)
+    {
+      enteredSecondHigh = true;
+    }
+    timer2 += 1;
+  }
+  if (timer2 > time2SetZero)
+  {
+    freq2 = 0;//if no new pulses from reed switch- tire is still, set Feeq to 0
+  }
+}
 
